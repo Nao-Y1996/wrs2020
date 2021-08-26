6d39fd45b3cdcf66a790c04a19fc1927f15ea72e
Nao-Y1996 <0809n.ysoccer@gmail.com>
2021-08-26 18:38:51 +0900

    modified marker_tf.py

--- scripts/marker_tf.py
+++ scripts/marker_tf.py
@@ -43,45 +43,52 @@ rate = rospy.Rate(10)
 
 if __name__ == "__main__":
 
-    markerNames = ['ar_marker/14']
-    tfs_length = 5
+    markerNames = ['ar_marker/14','ar_marker/710','ar_marker/711','ar_marker/712']
+    # tfs_length = 1
 
-    markers_translations = np.array([[[0.0]*3] * tfs_length] * len(markerNames))
+    # markers_translations = np.array([[0.0]*3]  * len(markerNames))
     stable_markers_translations = np.array([[0.0]*3] * len(markerNames))
 
-    # markers_rotations = np.array([[[0.0]*4] * tfs_length] * len(markerNames))
-    # stable_markers_rotations = np.array([[0.0]*4] * len(markerNames))
+    # markers_rotations = np.array([[0.0]*4] * len(markerNames))
+    stable_markers_rotations = np.array([[0.0]*4] * len(markerNames))
+    for j in range(len(markerNames)):
+        print(j)
+        stable_markers_rotations[j][3]=1.0
 
-    count = 0
     while not rospy.is_shutdown():
-        print('------count------'+str(count))
+        print('-----------------------------------------------------------')
         for i, marker in enumerate(markerNames):
             try:
                 (trans,rot) = listener.lookupTransform("/map", marker, rospy.Time(0))
-
-                markers_translations[i][count] = trans # trans means [x,y,z]
-                stable_markers_translations[i] = np.mean(markers_translations[i], axis=0)
-
-                # markers_rotations[i][count] = rot 
-                # stable_markers_rotations[i] = np.mean(markers_rotations[i], axis=0)
-
+                stable_markers_translations[i] = trans
+                stable_markers_rotations[i] = rot
                 print(marker + ' exists.')
-            except :
-                print(marker + ' does not exist.')
-                # stable_markers_rotations[i][3]=1.0
-                # import traceback
-                # traceback.print_exc()
-        count += 1
-        if count == tfs_length:
-            count = 0
+            except:
+                # print(marker + ' does not exist.')
+                try:
+                    (trans,rot) = listener.lookupTransform("/map", 'stable_'+marker, rospy.Time(0))
+                    stable_markers_translations[i] = trans
+                    stable_markers_rotations[i] = rot
+                    print('stable_'+marker + ' exists.')
+                except :
+                    # stable_markers_rotations[i][3]=1.0
+                    # import traceback
+                    # traceback.print_exc()
+                    pass
 
-        print('-----------------------------------------------------------')
         # map基準でTFをbroadcast
         for i, marker in enumerate(markerNames):
             #-------------------------↓　ありえない姿勢？(rotation)になるとbroadcastが止まってしまうという問題がある　↓-------------------------------
             # br.sendTransform(stable_markers_translations[i], stable_markers_rotations[i], Time.now(), 'stable_'+marker, '/map') 
+            # br.sendTransform(stable_markers_translations[i], [0.46,-0.528, 0.53, -0.47], Time.now(), 'stable_'+marker, '/map')
+
             #-----------------------------------------------------------------------------------------------------------------------------
-            br.sendTransform(stable_markers_translations[i], [0.46,-0.528, 0.53, -0.47], Time.now(), 'stable_'+marker, '/map')
+            try :
+                # br.sendTransform(stable_markers_translations[i], [0.46,-0.528, 0.53, -0.47], Time.now(), 'stable_'+marker, '/map')
+                br.sendTransform(stable_markers_translations[i],stable_markers_rotations[i], Time.now(), 'stable_'+marker, '/map')
+            except tf.DENORMALIZED_QUATERNION:
+                print('=='*20)
+                pass
         rate.sleep()
         
             
