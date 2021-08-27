d8e35a85445b03b89ee016d8c30cc22ad9dfcaf7
Nao-Y1996 <0809n.ysoccer@gmail.com>
2021-08-27 13:52:22 +0900

    update tf_memorizing.py

--- scripts/tf_memorizing.py
+++ scripts/tf_memorizing.py
@@ -12,7 +12,7 @@ br = TF.TransformBroadcaster()
 rate = rospy.Rate(10)
 
     
-TFs = ['ar_marker/14','ar_marker/710','ar_marker/711','ar_marker/712'] # all TF that you want to memorize
+TFs = ['ar_marker/709','ar_marker/710','ar_marker/711','ar_marker/712'] # all TF that you want to memorize
 
 memorized_tf_translations = np.array([[0.0]*3] * len(TFs))
 memorized_tf_rotations = np.array([[0.0]*4] * len(TFs))
@@ -29,26 +29,24 @@ if __name__ == "__main__":
                 trans, rot = listener.lookupTransform("/map", tf, rospy.Time(0))
                 memorized_tf_translations[i] = trans
                 memorized_tf_rotations[i] = rot
-                # br.sendTransform(memorized_tf_translations[i], memorized_tf_rotations[i],rospy. Time.now(), 'memorized_'+tf, '/map') ここでbroadcastするほうがループ処理が早いがCtr+Cで止まらないので面倒
+                br.sendTransform(memorized_tf_translations[i], memorized_tf_rotations[i],rospy. Time.now(), 'memorized_'+tf, '/map')
                 print(tf + ' exists.')
             except:
                 try:
                     trans, rot = listener.lookupTransform("/map", 'memorized_'+tf, rospy.Time(0))
                     memorized_tf_translations[i] = trans
                     memorized_tf_rotations[i] = rot
-                    # br.sendTransform(memorized_tf_translations[i], memorized_tf_rotations[i], rospy.Time.now(), 'memorized_'+tf, '/map') ここでbroadcastするほうがループ処理が早いがCtr+Cで止まらないので面倒
+                    br.sendTransform(memorized_tf_translations[i], memorized_tf_rotations[i], rospy.Time.now(), 'memorized_'+tf, '/map')
                 except TF.LookupException:
                     pass
                 except :
                     print('======== 【Unexpected Error】 ========\n======== check the code! ========')
                     import traceback
                     traceback.print_exc()
-        # map基準でTFをbroadcast
-        for i, tf in enumerate(TFs):
-            #-------------------------↓　ありえないrotationになるとbroadcastが止まってしまうのでコードを変更する際は注意すること　↓-------------------------------
-            br.sendTransform(memorized_tf_translations[i], memorized_tf_rotations[i], rospy.Time.now(), 'memorized_'+tf, '/map') 
-            # br.sendTransform(memorized_tf_translations[i], [0.46,-0.528, 0.53, -0.47], rospy.Time.now(), 'memorized_'+tf, '/map')
-            #----------------------------------------------------------------------------------------------------------------------------------------
+
+        #--------------------↓　以下のようにありえないrotationになるとbroadcastが止まってしまうのでコードを変更する際は注意すること　↓--------------------------
+        # br.sendTransform(memorized_tf_translations[i], [0.0, 0.0, 0.0, 0.0], rospy.Time.now(), 'memorized_'+tf, '/map')
+        #----------------------------------------------------------------------------------------------------------------------------------------
         rate.sleep()
         
             
